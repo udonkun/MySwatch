20d6eb5989054f2bee2be33907d7844e77544681
banseis <banseis75@gmail.com>
2018-02-12 20:28:45 +0900

    update stopwatch function

--- app/src/main/java/edu/self/myswatch/MainActivity.kt
+++ app/src/main/java/edu/self/myswatch/MainActivity.kt
@@ -2,11 +2,57 @@ package edu.self.myswatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+    private var timeValue = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val runnable = object : Runnable {
+            override fun run() {
+                timeValue++
+                timeToText(timeValue)?.let {
+                    timeText.text = it
+                }
+                handler.postDelayed(this, 1000)
+            }
+        }
+
+        startButton.setOnClickListener {
+            if (timeValue==0){
+                handler.post(runnable)
+            }
+        }
+        stopButton.setOnClickListener {
+            handler.removeCallbacks(runnable)
+        }
+        resetButton.setOnClickListener {
+            handler.removeCallbacks(runnable)
+            timeValue = 0
+            timeToText()?.let {
+                timeText.text = it
+            }
+        }
+
+    }
+
+    private fun timeToText(time: Int = 0):String? {
+        return if (time < 0) {
+            null
+        }else if (time == 0) {
+            "00:00:00"
+        }else {
+            val h = time / 3600
+            val m = time % 3600 / 60
+            val s = time % 60
+            "%1$02d:%2$02d:%3$02d".format(h, m , s)
+        }
     }
 }
+
